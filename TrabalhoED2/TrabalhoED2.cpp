9f8ce5fb3990451d646a47545e90f34f59713bb3
Luiz Guilherme <luiiz_jf@hotmail.com>
2022-11-11 21:23:28 -0300

    Cmake

---
+++ TrabalhoED2/TrabalhoED2.cpp
@@ -0,0 +1,12 @@
+﻿// TrabalhoED2.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "TrabalhoED2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
